20bc79e0419677d2dcde16d8bb4a8660010e5424
Shaun Ricketts <shaun.ricketts@defra.gov.uk>
2022-06-20 10:40:00 +0100

    PR amend

--- features/step_definitions/start-page.steps.js
+++ features/step_definitions/start-page.steps.js
@@ -2,7 +2,7 @@ const { Given, When, Then } = require("@wdio/cucumber-framework");
 
 const StartPage = require("../page_objects/start");
 
-Then("I should see the header with text saying {string}", async (message) => {
+Then("I should see the header text {string}", async (message) => {
   expect(StartPage.getHeading).toBeExisting();
   expect(StartPage.getHeading).toHaveTextContaining(message);
 });
