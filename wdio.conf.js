ae7e8b3d36a5a8e0030774aba611c3a3f3338b13
Shaun Ricketts <shaun.ricketts@defra.gov.uk>
2022-06-29 15:31:26 +0100

    Add local build baseurl

--- wdio.conf.js
+++ wdio.conf.js
@@ -98,8 +98,8 @@ exports.config = {
   // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
   // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
   // gets prepended directly.
-  baseUrl:
-    "https://biodiversity:netgain@bng-prototype.herokuapp.com/register-application/v9/",
+  baseUrl: "http://localhost:3000",
+  //"https://biodiversity:netgain@bng-prototype.herokuapp.com/register-application/v9/",
   //
   // Default timeout for all waitFor* commands.
   waitforTimeout: 10000,
