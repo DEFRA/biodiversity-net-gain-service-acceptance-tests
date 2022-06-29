62448e5dbcf0ce7a25b25f475fa49c916edc6c77
Shaun Ricketts <shaun.ricketts@defra.gov.uk>
2022-06-29 16:19:44 +0100

    add options

--- features/step_definitions/land-boundary.steps..js
+++ features/step_definitions/land-boundary.steps..js
@@ -6,16 +6,11 @@ Then(
   async (fileType) => {
     switch (fileType) {
       case "geospatial":
-        //await expect(uploads.geospatialOptionLbl).toContain(fileType);
-        uploads.selectUploadTypeGeospatial();
-        //(await uploads.geospatialOption).click();
+        (await uploads.geospatialOption).click();
         break;
       case "Document or Image":
-        //await expect(uploads.imageOptionLbl).toContain(fileType);
-        uploads.selectUploadTypeImage();
+        (await uploads.ImageOption).click();
         break;
-      // default:
-      //   console.log("Invalid option " + fileType);
     }
   }
 );
