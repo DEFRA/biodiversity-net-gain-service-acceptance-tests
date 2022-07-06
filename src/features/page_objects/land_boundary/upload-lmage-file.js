3064a6faedf37bac71d31518690e8da81dd8deeb
Shaun Ricketts <shaun.ricketts@defra.gov.uk>
2022-07-06 14:31:43 +0100

    merge changes

--- src/features/page_objects/land_boundary/upload-lmage-file.js
+++
@@ -1,9 +0,0 @@
-const Page = require("../base-page");
-
-class LandBoundaryUploadImageFile extends Page {
-  open() {
-    return super.open("/land/upload-land-boundary-file");
-    // return super.open("land/upload-image-file");
-  }
-}
-module.exports = new LandBoundaryUploadImageFile();
