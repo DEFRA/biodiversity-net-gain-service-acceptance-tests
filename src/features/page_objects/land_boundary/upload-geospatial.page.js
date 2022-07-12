9a27a7f48c4c09f95ff9dea62c981d23154b8287
Shaun Ricketts <shaun.ricketts@defra.gov.uk>
2022-07-12 16:14:48 +0100

    add temp geospatial page titles

--- src/features/page_objects/land_boundary/upload-geospatial.page.js
+++ src/features/page_objects/land_boundary/upload-geospatial.page.js
@@ -1,6 +1,10 @@
 const Page = require("../page");
 
 class LandBoundaryUploadGeospatial extends Page {
+  get Title() {
+    return "Register land for off-site biodiversity gain - GOV.UK";
+  }
+
   open() {
     return super.open("land/upload-geospatial-file");
   }
--- src/features/page_objects/land_boundary/upload-lmage-file.page.js
+++ src/features/page_objects/land_boundary/upload-lmage-file.page.js
@@ -1,6 +1,10 @@
 const Page = require("../page");
 
 class LandBoundaryUploadImageFile extends Page {
+  get Title() {
+    return "";
+  }
+
   open() {
     return super.open("/land/upload-land-boundary-file");
   }
