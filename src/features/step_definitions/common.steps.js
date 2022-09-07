aab53f528613c121d1b0f2ed8a03c606c4b6dd9e
Shaun Ricketts <shaun.ricketts@defra.gov.uk>
2022-09-07 14:39:54 +0100

    add management plan common steps

--- src/features/step_definitions/common.steps.js
+++ src/features/step_definitions/common.steps.js
@@ -2,14 +2,22 @@ const { Given, Then } = require("@wdio/cucumber-framework");
 const StartPage = require("../page_objects/start.page");
 const LegalAgreementUpload = require("../page_objects/legal_agreement/legal-agreement-upload.page");
 const LegalAgreementCheck = require("../page_objects/legal_agreement/legal-agreement-check.page");
+const ManagementPlanUpload = require("../page_objects/management_plan/management-plan-upload.page");
+const ManagementPlanCheck = require("../page_objects/management_plan/management-plan-check.page");
 const TaskListPage = require("../page_objects/task-list.page");
 const LandBoundaryChooseUploadOption = require("../page_objects/land_boundary/choose-upload-option.page");
 const LandBoundaryUploadGeospatial = require("../page_objects/land_boundary/upload-geospatial.page");
-const LandBoundaryUploadImageFile = require("../page_objects/land_boundary/upload-lmage-file.page");
+const LandBoundaryUploadImageFile = require("../page_objects/land_boundary/upload-land-boundary-file.page");
+const legalAgreementUploadPage = require("../page_objects/legal_agreement/legal-agreement-upload.page");
+
+const basePage = legalAgreementUploadPage;
+
 const pages = {
   start: StartPage,
   "legal-agreement-upload": LegalAgreementUpload,
   "legal-agreement-check": LegalAgreementCheck,
+  "management-plan-upload": ManagementPlanUpload,
+  "management-plan-check": ManagementPlanCheck,
   "task-list": TaskListPage,
   "location-options": LandBoundaryChooseUploadOption,
   "upload-geospatial-file": LandBoundaryUploadGeospatial,
@@ -30,4 +38,14 @@ Given(/^I (?:am on|navigate to) the "(.*)" page$/, async (page) => {
 Then(/^I should be (?:on|returned to) the "(.*)" page$/, async (page) => {
   // assert against the page title
   await expect(await browser.getTitle()).toContain(pages[page].titleText);
+});
+
+Then("I continue without an action", async () => {
+  // click continue
+  await basePage.continueButton.click();
+});
+
+Then("I should see the error {string}", async (message) => {
+  // check errorMsg text
+  await expect(basePage.errorMsg).toHaveTextContaining(message);
 });
