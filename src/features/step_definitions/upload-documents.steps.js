66a99e20f32874fdf1944d5b2f06c81e92c5104c
Shaun Ricketts <89984496+shaundefra@users.noreply.github.com>
2022-09-22 14:37:27 +0100

    Bngp 439 upload biodiversity metric file (#47)
    * add test files
    
    * add metric tests
    
    * touch
    
    * add metric to feature and filetype regression

--- src/features/step_definitions/upload-documents.steps.js
+++ src/features/step_definitions/upload-documents.steps.js
@@ -1,14 +1,17 @@
-const { When, Then } = require("@wdio/cucumber-framework");
+import { Given, When, Then } from "@cucumber/cucumber";
+import { join } from "path";
+import managementPlanUploadPage from "../page_objects/management_plan/management-plan-upload.page";
+import managementPlanCheckPage from "../page_objects/management_plan/management-plan-check.page";
+import legalAgreementUploadPage from "../page_objects/legal_agreement/legal-agreement-upload.page";
+import legalAgreementCheckPage from "../page_objects/legal_agreement/legal-agreement-check.page";
+import landBoundaryFileUploadPage from "../page_objects/land_boundary/upload-land-boundary.page";
+import landBoundaryFileCheckPage from "../page_objects/land_boundary/check-land-boundary-file.page";
+import metricUploadPage from "../page_objects/metric/metric-upload.page";
+import metricCheckPage from "../page_objects/metric/metric-check.page";
+import { assert } from "console";
+import { get } from "http";
 
-const path = require("path");
-const managementPlanUploadPage = require("../page_objects/management_plan/management-plan-upload.page");
-const managementPlanCheckPage = require("../page_objects/management_plan/management-plan-check.page");
-const legalAgreementUploadPage = require("../page_objects/legal_agreement/legal-agreement-upload.page");
-const legalAgreementCheckPage = require("../page_objects/legal_agreement/legal-agreement-check.page");
-const landBoundaryFileUploadPage = require("../page_objects/land_boundary/upload-land-boundary.page");
-const landBoundaryFileCheckPage = require("../page_objects/land_boundary/check-land-boundary-file.page");
-
-let uploadPage = legalAgreementUploadPage;
+let UploadPage = legalAgreementUploadPage;
 let CheckPage = legalAgreementCheckPage;
 
 let filename = "";
@@ -16,26 +19,35 @@ let filePath = "";
 let remoteFilePath = "";
 
 When("I choose and upload a {string}", async (document) => {
+  //default test file
+  filePath = join(__dirname, "../../TestFiles/test_12kb.docx");
+
   switch (document) {
     case "legal-agreement": {
-      uploadPage = legalAgreementUploadPage;
+      UploadPage = legalAgreementUploadPage;
       CheckPage = legalAgreementCheckPage;
       break;
     }
     case "management-plan": {
-      uploadPage = managementPlanUploadPage;
+      UploadPage = managementPlanUploadPage;
       CheckPage = managementPlanCheckPage;
       break;
     }
     case "land-boundary": {
-      uploadPage = landBoundaryFileUploadPage;
+      UploadPage = landBoundaryFileUploadPage;
       CheckPage = landBoundaryFileCheckPage;
+      break;
+    }
+    case "metric": {
+      UploadPage = metricUploadPage;
+      CheckPage = metricCheckPage;
+
+      //metric is .xlsx and .xslm files only
+      filePath = join(__dirname, "../../TestFiles/test_12kb.xlsx");
       break;
     }
   }
 
-  // test file
-  filePath = path.join(__dirname, "../../TestFiles/test_12kb.docx");
   remoteFilePath = await browser.uploadFile(filePath);
 
   // get the filename for assertions
@@ -43,10 +55,61 @@ When("I choose and upload a {string}", async (document) => {
   filename = group[group.length - 1];
 
   // open the upload url page
-  browser.url(uploadPage.path);
+  browser.url(UploadPage.path);
 
-  await uploadPage.govFileUpload.setValue(remoteFilePath);
-  await uploadPage.continueButton.click();
+  await UploadPage.govFileUpload.setValue(remoteFilePath);
+  await UploadPage.continueButton.click();
+});
+
+Given(/^I should be able to upload all allowed filetypes$/, async (table) => {
+  const tableRows = table.hashes();
+  for (const element of tableRows) {
+    switch (element.document) {
+      case "legal-agreement": {
+        UploadPage = legalAgreementUploadPage;
+        CheckPage = legalAgreementCheckPage;
+        break;
+      }
+      case "management-plan": {
+        UploadPage = managementPlanUploadPage;
+        CheckPage = managementPlanCheckPage;
+        break;
+      }
+      case "land-boundary": {
+        UploadPage = landBoundaryFileUploadPage;
+        CheckPage = landBoundaryFileCheckPage;
+        break;
+      }
+      case "metric": {
+        UploadPage = metricUploadPage;
+        CheckPage = metricCheckPage;
+        break;
+      }
+    }
+
+    await UploadPage.open();
+    await $("h1").waitForExist({ timeout: 5000 });
+
+    // test file, appended with the filetype
+    filePath = join(__dirname, "../../TestFiles/test_12kb." + element.filetype);
+
+    remoteFilePath = await browser.uploadFile(filePath);
+
+    // get the filename for assertions
+    var group = filePath.split("\\");
+    filename = group[group.length - 1];
+
+    // open the upload url page
+    browser.url(UploadPage.path);
+
+    await UploadPage.govFileUpload.setValue(remoteFilePath);
+    await UploadPage.continueButton.click();
+
+    await $("h1").waitForExist({ timeout: 5000 });
+
+    // assert against the page title
+    await expect(await browser.getTitle()).toContain(CheckPage.titleText);
+  }
 });
 
 Then("There should be a link to download the document", async () => {
@@ -70,33 +133,43 @@ Then("I should be able to see the filesize of the document", async () => {
 });
 
 When("I choose a file type that is not in the specified format", async () => {
-  const filePath = path.join(__dirname, "../../TestFiles/test.txt");
+  const filePath = join(__dirname, "../../TestFiles/test.txt");
   const remoteFilePath = await browser.uploadFile(filePath);
 
   // open the upload url page
-  browser.url(uploadPage.path);
+  browser.url(UploadPage.path);
 
-  await uploadPage.govFileUpload.setValue(remoteFilePath);
-  await uploadPage.continueButton.click();
+  await UploadPage.govFileUpload.setValue(remoteFilePath);
+  await UploadPage.continueButton.click();
 });
 
 Then("I should not be able to upload the file", async () => {
   // should still be on the upload page instead of the check page
   expect(await browser.getTitle()).not.toContain(CheckPage.titleText);
+});
 
+Then("I am informed that the file is empty", async () => {
   // wait for error message
-  await uploadPage.errorMsg.waitForExist({ timeout: 5000 });
+  await UploadPage.errorMsg.waitForExist({ timeout: 5000 });
+
+  await expect(UploadPage.errorMsg).toHaveTextContaining(
+    "The selected file is empty"
+  );
 });
 
 Then("I am informed of what the allowed file types should be", async () => {
   // wait for error message
-  await (await uploadPage.errorMsg).waitForDisplayed({ timeout: 5000 });
+  await (await UploadPage.errorMsg).waitForDisplayed({ timeout: 5000 });
 
   let errorTxt = "";
 
-  switch (uploadPage) {
+  switch (UploadPage) {
     case landBoundaryFileUploadPage: {
       errorTxt = "The selected file must be a DOC, DOCX, JPG, PNG or PDF";
+      break;
+    }
+    case metricUploadPage: {
+      errorTxt = "The selected file must be an XLSM or XLSX";
       break;
     }
     default: {
@@ -105,7 +178,7 @@ Then("I am informed of what the allowed file types should be", async () => {
   }
 
   // check errorMsg text
-  await expect(uploadPage.errorMsg).toHaveTextContaining(errorTxt);
+  await expect(UploadPage.errorMsg).toHaveTextContaining(errorTxt);
 });
 
 When("I choose a different file", async () => {
@@ -115,7 +188,16 @@ When("I choose a different file", async () => {
 });
 
 When("I choose an empty file", async () => {
-  filePath = path.join(__dirname, "../../TestFiles/test_1k_empty.pdf");
+  switch (UploadPage) {
+    case metricUploadPage: {
+      filePath = join(__dirname, "../../TestFiles/test_1k_empty.xlsx");
+      break;
+    }
+    default: {
+      filePath = join(__dirname, "../../TestFiles/test_1k_empty.pdf");
+    }
+  }
+
   remoteFilePath = await browser.uploadFile(filePath);
 
   // get the filename for assertions
@@ -123,10 +205,10 @@ When("I choose an empty file", async () => {
   filename = group[group.length - 1];
 
   // open the upload url page
-  browser.url(uploadPage.path);
+  browser.url(UploadPage.path);
 
-  await uploadPage.govFileUpload.setValue(remoteFilePath);
-  await uploadPage.continueButton.click();
+  await UploadPage.govFileUpload.setValue(remoteFilePath);
+  await UploadPage.continueButton.click();
 });
 
 Then("The original document should be deleted", async function () {
