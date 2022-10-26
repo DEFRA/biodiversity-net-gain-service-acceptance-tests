f652dd612dfc3b1c8fd7cd1ae9d741440bc89207
Shaun Ricketts <shaun.ricketts@defra.gov.uk>
2022-10-26 14:49:44 +0100

    add generic donot have document selector

--- src/features/page_objects/page.js
+++ src/features/page_objects/page.js
@@ -35,6 +35,10 @@ module.exports = class Page {
     return $("#filesize");
   }
 
+  get doNotHaveDocument() {
+    return $("#dont-have-document");
+  }
+
   /**
    * Opens a sub page of the page
    * @param path path of the sub page (e.g. /path/to/page.html)
