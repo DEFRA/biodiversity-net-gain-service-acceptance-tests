2ef5d014dfe0273742826ff38b6c618f278215ff
Shaun Ricketts <shaun.ricketts@defra.gov.uk>
2023-05-05 17:57:39 +0100

    linting update

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict'
 // wdio.conf.js is the default configuration file used when a specific configuration is not referenced on the command line.
 // By default, run with the local configuration
-exports.config = require("./src/conf/wdio.local.conf").config;
+exports.config = require('./src/conf/wdio.local.conf').config
