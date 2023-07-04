0ebddc9af4ec8c1fbf0e5e20701f68dc9a25d8dd
Shaun Ricketts <shaun.ricketts@defra.gov.uk>
2023-07-04 15:46:53 +0100

    update login steps

--- src/features/step_definitions/login.steps.js
+++ src/features/step_definitions/login.steps.js
@@ -2,9 +2,9 @@ import { When } from "@cucumber/cucumber";
 import LoginPage from "../page_objects/login.page";
 
 When ("I login to the Government Gateway", async () => {
-    await (LoginPage.govGatewayUserID).addValue("894836023882");
-    await (LoginPage.govGatewayPassword).addValue("ChristopherWallace");
-    await (LoginPage.govGatewaySignInBtn).click();
+    await (LoginPage.UserID).addValue("894836023882");
+    await (LoginPage.Password).addValue("ChristopherWallace");
+    await (LoginPage.SignInBtn).click();
 
     /* set a wait for the gove gateway creds to redirect back to the service 
     todo: this is very flaky and not sure its the best way to achieve this */
