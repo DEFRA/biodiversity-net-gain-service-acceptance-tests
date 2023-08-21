a7899cb9c2ace68ed385e2a1d21980febc16baa1
Shaun Ricketts <shaun.ricketts@defra.gov.uk>
2023-08-21 10:48:43 +0100

    content changes

--- src/features/page_objects/land_boundary/upload-land-boundary.page.js
+++ src/features/page_objects/land_boundary/upload-land-boundary.page.js
@@ -2,7 +2,7 @@ const Page = require("../page");
 
 class LandBoundaryUploadImageFile extends Page {
   get titleText() {
-    return "Upload a document or image showing the land boundary";
+    return "Upload a document or image showing the biodiversity gain site boundary";
   }
 
   get path() {
