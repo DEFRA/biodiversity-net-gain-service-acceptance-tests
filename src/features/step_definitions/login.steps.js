f8633534a958a1c770f66c0efdd0686c3d1d1f6d
Shaun Ricketts <shaun.ricketts@defra.gov.uk>
2023-09-20 09:20:25 +0100

    reduce wait and add assertion for gov login page

--- src/features/step_definitions/login.steps.js
+++ src/features/step_definitions/login.steps.js
@@ -2,7 +2,7 @@ import { When } from "@cucumber/cucumber";
 import LoginPage from "../page_objects/login.page";
 
 When ("I login to the Government Gateway", async () => {
-    await $("h1").waitForExist({ timeout: 5000 });
+    await $("h1").waitForExist();
     // assert against the page title
     expect(await browser.getTitle()).toContain(LoginPage.titleText);
 
@@ -12,10 +12,14 @@ When ("I login to the Government Gateway", async () => {
   async function login(gatewayid, password) {
     await (LoginPage.UserID).addValue(gatewayid);
     await (LoginPage.Password).addValue(password);
-    await browser.pause(10000);
+    await browser.pause(5000);
     await (LoginPage.SignInBtn).click();
 
     /* set a wait for the gove gateway creds to redirect back to the service 
     todo: this is very flaky and not sure its the best way to achieve this */
-    await browser.pause(10000);
+    await browser.pause(5000);
+    var pageUrl = browser.getUrl();
+    expect(await pageUrl).not.toContain('https://www.ete.access.service.gov.uk/login/signin/creds')
 }
+
+
