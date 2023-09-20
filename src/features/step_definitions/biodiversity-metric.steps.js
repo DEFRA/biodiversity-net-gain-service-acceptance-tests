fa73d47f6b21c5c6a3967ed1262507cc6fa7ecfc
Shaun Ricketts <89984496+shaundefra@users.noreply.github.com>
2023-09-20 09:17:04 +0100

    datatestids and tests for metric display totals (#254)
    * datatestids and tests for metric display totals
    
    * todo for mulitple Ands
    
    * add regression tag to metric display tests

--- src/features/step_definitions/biodiversity-metric.steps.js
+++ src/features/step_definitions/biodiversity-metric.steps.js
@@ -1,13 +1,31 @@
-const { Given, When } = require('@wdio/cucumber-framework')
-const checkOwnershipDetailsPage = require('../page_objects/land_ownership/check-ownership-details.page')
-const registeredLandownerPage = require('../page_objects/land_ownership/registered-landowner.page')
+const { Given, Then } = require('@wdio/cucumber-framework')
+const basePage = require('../page_objects/metric/check-habitat-baseline.page')
+// use basepage to cover both check-habitat-baseline.page and check-habitat-created.page
+
+Then("The total for {string} should be {string}", async (HabitatType, total) => {
+  switch (HabitatType) {
+    case "Habitat type and condition": {
+      await expect(basePage.habitatTotal).toHaveTextContaining(total);
+      break;
+    }
+    case "Hedgerow type and condition": {
+      await expect(basePage.hedgeTotal).toHaveTextContaining(total);
+      break;
+    }
+    case "River type and condition": {
+      await expect(basePage.riverTotal).toHaveTextContaining(total);
+      break;
+    }
+  }
+})
 
 Given('I have completed the biodiversity metric section', async () => {
   await completeBiodiversityMetricSection()
 })
 
 async function completeBiodiversityMetricSection () {
-  // And I choose and upload a "metric" file
+  // todo: refine multiple ands
+  //And I choose and upload a "metric" file
   //       And I confirm it is the correct file
 
   //       And I check the habitat baseline information is correct
