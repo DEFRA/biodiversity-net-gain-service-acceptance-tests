9f79905c8625e8be6f57320d456a444b733d79cd
Shaun Ricketts <shaun.ricketts@defra.gov.uk>
2023-09-21 14:41:05 +0100

    add login steps

--- src/features/login.feature
+++ src/features/login.feature
@@ -1,6 +1,4 @@
-
 Feature: Login
-
     As an Authenticated User
     I need to be directed to/ from the login page
     So that I can continue with my journey
@@ -9,6 +7,7 @@ Feature: Login
         Given I navigate to the "start" page
         And I start my registration
         When I login to the Government Gateway
+        Then I am logged in to the service
 # Then I should be returned to the "start" page
 # Then I should be returned to the "register-land-task-list" page
 # Then I should be returned to the "register-land-task-list" page
--- src/features/step_definitions/login.steps.js
+++ src/features/step_definitions/login.steps.js
@@ -1,25 +1,31 @@
-import { When } from "@cucumber/cucumber";
+import { When, Then } from "@cucumber/cucumber";
 import LoginPage from "../page_objects/login.page";
 
 When ("I login to the Government Gateway", async () => {
     await $("h1").waitForExist();
+    
     // assert against the page title
     expect(await browser.getTitle()).toContain(LoginPage.titleText);
 
     await login("894836023882", "ChristopherWallace");
   })
 
+  Then("I am logged in to the service", async () => {
+    /* set a wait for the gove gateway creds to redirect back to the service 
+    todo: this is very flaky and not sure its the best way to achieve this */    
+    await browser.pause(5000);
+
+    //check that we are logged in and not still on the sign in page
+    var pageUrl = browser.getUrl();
+    expect(await pageUrl).not.toContain('https://www.ete.access.service.gov.uk/login/signin/creds')
+  })
+
   async function login(gatewayid, password) {
     await (LoginPage.UserID).addValue(gatewayid);
     await (LoginPage.Password).addValue(password);
+    
     await browser.pause(5000);
     await (LoginPage.SignInBtn).click();
-
-    /* set a wait for the gove gateway creds to redirect back to the service 
-    todo: this is very flaky and not sure its the best way to achieve this */
-    await browser.pause(5000);
-    var pageUrl = browser.getUrl();
-    expect(await pageUrl).not.toContain('https://www.ete.access.service.gov.uk/login/signin/creds')
 }
 
 
