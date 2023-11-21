ad1dc116721f363d5e11d581c4a361502765b5f8
Shaun Ricketts <shaun.ricketts@defra.gov.uk>
2023-11-21 16:56:29 +0000

    landownership e2e steps

--- src/features/step_definitions/land-ownership.steps.js
+++ src/features/step_definitions/land-ownership.steps.js
@@ -1,22 +1,42 @@
 const { Given, When } = require("@wdio/cucumber-framework");
 const landOwnershipProofListPage = require("../page_objects/land_ownership/ownership-proof-list.page");
 const tasklistPage = require("../page_objects/register-land-task-list.page");
+const addLandownersPage = require("../page_objects/land_ownership/add-landowners.page");
+const landOwnerConsentPage = require("../page_objects/land_ownership/land-owner-consent.page");
+const landOwnershipCheckDetailsPage = require("../page_objects/land_ownership/check-ownership-details.page");
 
 Given("I have completed the land-ownership section", async () => {
-    await completeLandOwnershipSection();
+    await completeLandOwnershipSection("John Smith",);
   })
 
-async function completeLandOwnershipSection() {
+async function completeLandOwnershipSection(fullname) {
 
-  
-  //And I confirm i have added allproof of landownership files
-  // multiple file addition page_object needs adding
-  // assert against the page title
+
+  //And I add all registered landowners 
   await $("h1").waitForExist({ timeout: 5000 });
-  expect(await browser.getTitle()).toContain(landOwnershipProofListPage.titleText);
+  expect(await browser.getTitle()).toContain(addLandownersPage.titleText);
 
-  await landOwnershipProofListPage.radioYes.click();
-  await landOwnershipProofListPage.continueButton.click();
+  await addLandownersPage.addlandowner(fullname);
+  await addLandownersPage.continueButton.click();
+
+  // //And I confirm i have added allproof of landownership files
+  // // multiple file addition page_object needs adding
+  // // assert against the page title
+  // await $("h1").waitForExist({ timeout: 5000 });
+  // expect(await browser.getTitle()).toContain(landOwnershipProofListPage.titleText);
+  // await landOwnershipProofListPage.radioYes.click();
+  // await landOwnershipProofListPage.continueButton.click();
+
+
+  //Landowner consent
+  await $("h1").waitForExist({ timeout: 5000 });
+  expect(await browser.getTitle()).toContain(landOwnerConsentPage.titleText);
+  await landOwnerConsentPage.landownerConsentChkBox.click();
+  await landOwnerConsentPage.continueButton.click();
+
+  //Check the landownership details
+  expect(await browser.getTitle()).toContain(landOwnershipCheckDetailsPage.titleText);
+  await landOwnershipCheckDetailsPage.continueButton.click();
 
   //tasklist section shows as complete
   expect(await browser.getTitle()).toContain(tasklistPage.titleText);
