18311861f226bf3f036ccc9f75deef48e7816ea9
Shaun Ricketts <shaun.ricketts@defra.gov.uk>
2023-12-03 19:35:53 +0000

    move login function to PO

--- src/features/page_objects/login.page.js
+++ src/features/page_objects/login.page.js
@@ -17,6 +17,23 @@ class LoginPage extends Page {
     return $("#continue");
   }
 
+  async login(username, password) {
+    await this.UserID.setValue(username);
+    await this.Password.setValue(password);
+
+    await browser.pause(5000);
+    await this.SignInBtn.click();
+  }
+
+//   async function login(gatewayid, password) {
+//     await (LoginPage.UserID).addValue(gatewayid);
+//     await (LoginPage.Password).addValue(password);
+    
+//     await browser.pause(5000);
+//     await (LoginPage.SignInBtn).click();
+// }
+
+
   open() {
     return super.open("https://www.ete.access.service.gov.uk/login/signin/creds");
   }
