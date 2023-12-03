e891730a3c623f4f090c0d55cfc86a901665e05d
Shaun Ricketts <shaun.ricketts@defra.gov.uk>
2023-12-03 19:35:24 +0000

    individual user for login

--- src/features/step_definitions/login.steps.js
+++ src/features/step_definitions/login.steps.js
@@ -1,13 +1,15 @@
 const { Given, When, Then } = require('@wdio/cucumber-framework');
 const LoginPage = require("../page_objects/login.page");
+const TaskListPage = require("../page_objects/register-land-task-list.page");
+
 
 When ("I login to the Government Gateway", async () => {
     await $("h1").waitForExist();
     
     // assert against the page title
     expect(await browser.getTitle()).toContain(LoginPage.titleText);
-
-    await login("894836023882", "ChristopherWallace");
+    
+    await LoginPage.login("528250494194", "ChristopherWallace");
   })
 
   Then("I am logged in to the service", async () => {
@@ -18,14 +20,15 @@ When ("I login to the Government Gateway", async () => {
     //check that we are logged in and not still on the sign in page
     var pageUrl = browser.getUrl();
     expect(await pageUrl).not.toContain('https://www.ete.access.service.gov.uk/login/signin/creds')
-  })
+    expect(await pageUrl).not.toContain('https://dcidmtest.b2clogin.com')
 
-  async function login(gatewayid, password) {
-    await (LoginPage.UserID).addValue(gatewayid);
-    await (LoginPage.Password).addValue(password);
-    
-    await browser.pause(5000);
-    await (LoginPage.SignInBtn).click();
-}
+
+    //If a new application then will redirect to the tasklist page Todo: get the Jira page
+    // #Landing page - tasklist for new session (TODO refine after)
+    // as random landing at the moment
+    // await $("h1").waitForExist();
+    // expect(await browser.getTitle()).toContain(TaskListPage.titleText);
+
+  })
 
 
