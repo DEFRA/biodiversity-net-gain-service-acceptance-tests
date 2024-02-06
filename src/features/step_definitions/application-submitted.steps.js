d7d03d454637e85800d03c8f7e035c36d107fd2c
Shaun Ricketts <89984496+shaundefra@users.noreply.github.com>
2024-02-06 18:23:08 +0000

    Develop (#277)
    * remove the hmmp section from e2e
    
    * individual user for login
    
    * move login function to PO
    
    * remove test tag
    
    * remove unecessary comment
    
    * update legal agreement tests
    
    * update enddate page name
    
    * BNGP-4137 Enter landowner details content
    
    * delet add-landonwers
    
    * remove timeout on title assert
    
    * remove unecessary backgrounds
    
    * regression merge
    
    * BNGP-4424-5  service name
    
    * BNGP-4424-5 service title update
    
    * BNGP-4424-5 service title update
    
    * update tasklist status
    
    * remove eligibility questions from the reg journey tests
    
    * remove hmmp reg journey tests
    
    * remove redundant tags
    
    * update local config for defraid
    
    * legal agreement updates
    
    * remove redundant const
    
    * touch
    
    * add service url to beforeFeature
    
    * Feature/bngp 4486 remove legacy front end application level start page (#274)
    
    * https://eaflood.atlassian.net/browse/BNGP-4486 update before each login to signin not start
    
    * remove before feature
    
    * BNGP-4486 remove legacy start page
    
    * remove legacy startpage refs
    
    * Feature/bngp 4336 bacs update all environments with natural england account details for payment (#276)
    
    * Develop (#275)
    
    * remove the hmmp section from e2e
    
    * individual user for login
    
    * move login function to PO
    
    * remove test tag
    
    * remove unecessary comment
    
    * update legal agreement tests
    
    * update enddate page name
    
    * BNGP-4137 Enter landowner details content
    
    * delet add-landonwers
    
    * remove timeout on title assert
    
    * remove unecessary backgrounds
    
    * regression merge
    
    * BNGP-4424-5  service name
    
    * BNGP-4424-5 service title update
    
    * BNGP-4424-5 service title update
    
    * update tasklist status
    
    * remove eligibility questions from the reg journey tests
    
    * remove hmmp reg journey tests
    
    * remove redundant tags
    
    * update local config for defraid
    
    * legal agreement updates
    
    * remove redundant const
    
    * touch
    
    * add service url to beforeFeature
    
    * Feature/bngp 4486 remove legacy front end application level start page (#274)
    
    * https://eaflood.atlassian.net/browse/BNGP-4486 update before each login to signin not start
    
    * remove before feature
    
    * BNGP-4486 remove legacy start page
    
    * remove legacy startpage refs
    
    * 4336 - BACS account tests
    
    * add bacs tests to e2e
    
    * Merge branch 'develop' into feature/BNGP-4336-BACS-update-all-environments-with-Natural-England-account-details-for-payment
    
    * merge conflict fix

--- src/features/step_definitions/application-submitted.steps.js
+++ src/features/step_definitions/application-submitted.steps.js
@@ -1,4 +1,4 @@
-const { Given, When, Then } = require("@wdio/cucumber-framework");
+const {Then} = require("@wdio/cucumber-framework");
 const applicationSubmittedPage = require("../page_objects/application-submitted.page");
 
 Then("The biodiversity gain site reference should be displayed", async () => {
@@ -7,5 +7,25 @@ Then("The biodiversity gain site reference should be displayed", async () => {
 });
 
 Then("The fee amount of {string} should be displayed", async(fee) =>{
+        await expect(applicationSubmittedPage.feeAmount).toBeDisplayed()
         await expect(applicationSubmittedPage.feeAmount).toHaveText(fee);
+})
+
+Then("The account number {string} should be displayed", async(account) =>{
+        await expect(applicationSubmittedPage.accountNumber).toBeDisplayed();
+        await expect(applicationSubmittedPage.accountNumber).toHaveText(account);
+})
+
+Then("The sort code {string} should be displayed", async(sortcode) =>{
+        await expect(applicationSubmittedPage.sortCode).toBeDisplayed();
+        await expect(applicationSubmittedPage.sortCode).toHaveText(sortcode);
+})
+
+Then("The swift code {string} should be displayed", async(swiftcode) =>{
+        await expect(applicationSubmittedPage.swiftBicCode).toBeDisplayed();
+        await expect(applicationSubmittedPage.swiftBicCode).toHaveText(swiftcode);
+})
+
+Then("the fee amount should not be displayed", async() =>{
+        await expect(applicationSubmittedPage.referenceNumber).not.toBeDisplayed();
 })
